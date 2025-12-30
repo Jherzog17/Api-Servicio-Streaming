7e8083b572b268eca37db3621423cb80a3cae7e4
raulguijarroc-474 <raul.guijarroc@gmail.com>
2025-12-30 22:11:21 +0100

    historial y modificaicon dele backend

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, Blueprint
 from flask_jwt_extended import JWTManager
 from users_bp import users_bp
 from media_bp import media_bp
+from historial_bp import historial_bp
 import os
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ jwt = JWTManager(app)
 
 app.register_blueprint(users_bp, url_prefix="/usuarios")
 app.register_blueprint(media_bp, url_prefix="/medio_de_streaming")
+app.register_blueprint(historial_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ historial_bp.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, Blueprint
+from marshmallow import Schema, fields, validate, ValidationError
+
+from users_bp import users_db
+from historial_db import historial
+
+historial_bp = Blueprint("historial", __name__)
+
+class HistorialItemSchema(Schema):
+    id_medio_de_streaming = fields.Int(required=True)
+    fecha_visualizacion = fields.Date(required=False)
+
+@historial_bp.route("/usuarios/<id_usuario>/historial", methods=["GET"])
+def consultar_historial(id_usuario):
+    # usuario existe ¿
+    if id_usuario not in users_db:
+        return jsonify({'error': 'Usuario no encontrado'}), 404
+    
+    # hisorial y si no hay lista vacia
+    lista_historial = historial.get(id_usuario, [])
+    return jsonify({"historial": lista_historial}), 200
+
+@historial_bp.route("/usuarios/<id_usuario>/historial", methods=["POST"])
+def anadir_al_historial(id_usuario):
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'Missing JSON'}), 400
+    try:
+        schema = HistorialItemSchema()
+        schema.load(data)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+
+    if id_usuario not in users_db:
+        return jsonify({'error': 'Usuario no encontrado'}), 404
+    # si no hay historial rntonces se crea la lista 
+    if id_usuario not in historial:
+        historial[id_usuario] = []
+    # + itm a la lista
+    historial[id_usuario].append(data)
+    
+    return jsonify({"mensaje": "Añadido al historial", "historial": historial[id_usuario]}), 201
